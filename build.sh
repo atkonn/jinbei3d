747d77386642427c7d6a6475ee7b32668d851211
Atsushi Konno <atkonn@gmail.com>
2011-07-21 09:48:14 +0900

    * Initial commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+ant clean
+ant debug
+adb uninstall jp.co.qsdn.android.jinbei3d && adb install bin/jinbei3d-debug.apk
---
+++ release.sh
@@ -0,0 +1,3 @@
+ant clean
+ant release
+adb uninstall jp.co.qsdn.android.jinbei3d && adb install bin/jinbei3d-release.apk
